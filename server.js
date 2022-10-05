b9fced82dd136d8508c645c302eae9b48e9d7a47
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-10-05 09:27:37 +0200

    Added api routes for POSTS and GET ALL Messages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js"
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
@@ -9,6 +10,7 @@ const app = express()
 const port = process.env.PORT || 9000
 
 // middleware
+app.use(express.json())
 
 // DB config
 let connectionUrl = `mongodb+srv://${process.env.REACT_APP_MONGO_DB}:${process.env.REACT_APP_MONGO_PASSWORD}@cluster0.vxakqit.mongodb.net/whatsappDB?retryWrites=true&w=majority`
@@ -21,5 +23,26 @@ mongoose.connect(connectionUrl, {
 
 // API routes
 app.get("/", (req, res) => res.status(200).send("hello friend.."))
+app.post("/api/v1/messages/new", (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(`New message created \n ${data}`)
+        }
+    })
+})
+app.get("/api/v1/messages/sync", (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
 // listener
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
